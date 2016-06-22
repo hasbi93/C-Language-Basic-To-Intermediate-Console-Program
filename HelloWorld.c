99092eb6f9e62e8f77311000c5835f6c77f6a60a
hasbi93 <muhammad.hasbi@protonmail.ch>
2016-06-22 13:44:06 +0700

    Hello World Console Program
    Basic Hello World Program Written in C Language

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+/*
+* Title               : Console Hello World Program (HelloWorld.c)
+* Program Description : Write a C Language To Print Hello World Program on The Screen.
+* Author              : Muhammad Hasbi, S.T.
+* Interface           : Console
+* IDE                 : Dev-C++ 5.11
+* Operating System    : Windows 8 Pro
+*/
+
+#include<stdio.h>
+
+main()
+{
+	printf("Hello World \n");
+}
